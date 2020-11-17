c51d7a73998caa8c0fd1d8e91c9ffae93f8fa665
Hans Philipp Zimmermann <hans.zimmermann@laroomy.de>
2020-11-17 07:59:49 +0100

    Create17-11-2020

---
+++ TV Extend Unit Controller/main.c
@@ -0,0 +1,18 @@
+/*
+ * TV Extend Unit Controller.c
+ *
+ * Created: 17.11.2020 07:44:09
+ * Author : hans-
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
